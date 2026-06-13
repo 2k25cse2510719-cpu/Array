bd46ab7100c739af792e1ce2ed9f0b936a28aeba
Ishan Chauhan <2k25cse2510719@gmail.com>
2026-06-13 22:21:28 +0530

    Smallest Element
    Finding the smallest element in an array.

---
+++ smallest.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+    int size;
+    cout<<"Enter Size : "<<endl;
+    cin>>size;
+    int arr[size];
+    cout<<"Enter Elements :"<<endl; 
+    for(int i=0;i<size;i++){
+        cin>>arr[i];
+    }
+    for(int i =0;i<size;i++)
+    {
+   cout<<arr[i]<<" "<<endl;
+    }
+    int smallest=arr[0];
+  for(int i =0 ;i<size;i++)
+  {
+    if(arr[i]<smallest)
+    {
+smallest=arr[i];
+    }
+  }
+cout<<"smallest : "<<endl<<smallest<<endl;
+}
