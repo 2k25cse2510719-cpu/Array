89562d5461637bbdc9db444a89949ff7a2d07caa
Ishan Chauhan <2k25cse2510719@gmail.com>
2026-06-15 10:41:14 +0530

    First Occurrence
    Finding the first occurrence of an element.

---
+++ occur2.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+    int size,ele,count=0;
+    cout<<"Enter Size : "<<endl;
+    cin>>size;
+    int arr[size];
+    cout<<"Enter Elements : "<<endl;
+    for(int i =0;i<size;i++)
+    {
+        cin>>arr[i];
+    }
+    for(int i =0;i<size;i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+    cout<<"Enter an element for occurrance = "<<endl;
+    cin>>ele;
+    cout<<"Element occured at ";
+  for(int i =0;i<size;i++)
+  {
+    if (arr[i]==ele)
+    {;
+cout<<"Occurred at "<<i+1;
+break;
+    }
+  }
+}
